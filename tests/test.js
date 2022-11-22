9ae5abbaa19967a4bd9542c83fd20dc16a45dcf9
Bar Nuri <barnuri@hotmail.com>
2022-11-22 15:35:49 +0200

    Create test.js

---
+++ tests/test.js
@@ -0,0 +1,3 @@
+if(BinData(3, 'S9p5wcj6SU63EGzCr72b0w==').toCSUUID().toBinData().toHex() != BinData(3, 'S9p5wcj6SU63EGzCr72b0w==').toHex()) {
+    throw new Error('toCSUUID\\toBinData\\toHex is broken');
+}
