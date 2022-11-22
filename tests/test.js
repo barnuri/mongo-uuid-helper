666864ac02911be96e7f36a25a65c848eace4eec
Bar Nuri <barnuri@hotmail.com>
2022-11-22 15:38:51 +0200

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,5 @@
 if(BinData(3, 'S9p5wcj6SU63EGzCr72b0w==').toCSUUID().toBinData().toHex() == BinData(3, 'S9p5wcj6SU63EGzCr72b0w==').toHex()) {
-    throw new Error('toCSUUID\\toBinData\\toHex is broken');
+    throw new Error('toCSUUID \\ toBinData \\ toHex is broken');
 }
 
 console.log("test passed");
